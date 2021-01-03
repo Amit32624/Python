e622044cd8d691897d155fc7d97210c4d5e38500
Amit32624 <120220153@umail.ucc.ie>
2021-01-03 21:31:16 +0000

    Status changed for printing

--- Python_codes/DemoComplex.py
+++ Python_codes/DemoComplex.py
@@ -8,4 +8,7 @@ print("Complex no C1 = "+str(c1))
 print("Real Part = "+str(c1.real))
 print("Imaginary Part= "+str(c1.imag))
 print("Conjugate = "+str(c1.conjugate()))
+print("Imaginary Part= "+str(c1.imag))
+print("Conjugate = "+str(c1.conjugate()))
+print("Conjugate = "+str(c1.conjugate()))
 
